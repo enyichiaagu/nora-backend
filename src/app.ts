17be536fb7fe41aaef9ac2238915bf09848c5142
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-22 23:37:50 +0000

    Updated app.ts

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import { createServer } from 'http';
+import express from 'express';
+import { WebSocketServer } from 'ws';
+import { AssemblyAI } from 'assemblyai';
+
+const client = new AssemblyAI({
+  apiKey: '5a1adfc2fa814c4a90c0f8110077d0db',
+});
+const transcriber = client.streaming.transcriber({
+  sampleRate: 16_000,
+  formatTurns: true,
+});
+
+const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', async function connection(ws) {
+  ws.on('error', console.error);
+  ws.on('message', function message(audioBuffer: ArrayBuffer) {
+    transcriber.sendAudio(audioBuffer);
+  });
+  ws.on('close', async () => {
+    console.log('Closing streaming transcript connection');
+    transcriber.close();
+  });
+
+  transcriber.on('open', ({ id }) => {
+    console.log(`Session opened with ID:${id}`);
+  });
+  transcriber.on('error', (error) => {
+    console.error('Error:', error);
+  });
+  transcriber.on('close', (code, reason) =>
+    console.log('Session closed:', code, reason)
+  );
+  transcriber.on('turn', (turn) => {
+    if (!turn.transcript) {
+      return;
+    }
+    ws.send(JSON.stringify({ transcript: turn.transcript }));
+  });
+
+  console.log('Connecting to streaming transcript service');
+  await transcriber.connect();
+  console.log('[AssemblyAI] Connected');
+});
+
+server.listen(3000, () => console.log('Started Backend Server'));
+wss.on('listening', () => console.log('[WS] Connection Open'));
