0892ca05a413cfe9fc0f812d6cc91caaaabf77b8
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-22 18:25:51 +0000

    Updated transcribe.utils.ts

--- src/transcribe/transcribe.utils.ts
+++ src/transcribe/transcribe.utils.ts
@@ -6,4 +6,57 @@ export async function start() {
   const client = new AssemblyAI({
     apiKey: process.env.ASSEMBLY_AI,
   });
+
+  const transcriber = client.streaming.transcriber({
+    sampleRate: 16_000,
+    formatTurns: true
+  });
+
+  transcriber.on("open", ({ id }) => {
+    console.log('Session opened with ID:${id}')
+  });
+
+  transcriber.on("error", (error) => {
+    console.error("Error:", error);
+  });
+
+  transcriber.on("close", (code, reason) =>
+    console.log("Session closed:", code, reason),
+  );
+
+  transcriber.on("turn", (turn) => {
+    if (!turn.transcript) {
+      return;
+    }
+    console.log("Turn:", turn.transcript);
+  });
+
+  try {
+    console.log("Connecting to streaming transcript service");
+
+    await transcriber.connect();
+
+    console.log("Starting recording");
+
+    const recording = recorder.record({
+      channels: 1,
+      sampleRate: 16_000,
+      audioType: "wav", // Linear PCM
+    });
+
+    Readable.toWeb(recording.stream()).pipeTo(transcriber.stream());
+
+    process.on("SIGINT", async function () {
+      console.log();
+      console.log("Stopping recording");
+      recording.stop();
+
+      console.log("Closing streaming transcript connection");
+      await transcriber.close();
+
+      process.exit();
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
