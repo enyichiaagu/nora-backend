fbf6b5e0299386451322a4716d690774b410917d
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-22 18:22:13 +0000

    Updated transcribe.utils.ts

--- src/transcribe/transcribe.utils.ts
+++ src/transcribe/transcribe.utils.ts
@@ -2,3 +2,8 @@ import { Readable } from 'stream'
 import { AssemblyAI } from 'assemblyai'
 import recorder from 'node-record-lpcm16'
 
+export async function start() {
+  const client = new AssemblyAI({
+    apiKey: process.env.ASSEMBLY_AI,
+  });
+}
