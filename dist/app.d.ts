87c3e3531a572db9137906c6dcd38034d0ebe7ef
enyichiaagu <enyichiaagu@gmail.com>
2025-06-27 10:21:35 +0100

    changes

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,2 +1,2 @@
-import 'dotenv/config';
+export {};
 //# sourceMappingURL=app.d.ts.map
--- dist/app.js
+++ dist/app.js
@@ -1,50 +1,61 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_http_1 = require("node:http");
-const express_1 = __importDefault(require("express"));
-const ws_1 = require("ws");
-const assemblyai_1 = require("assemblyai");
-require("dotenv/config");
-const client = new assemblyai_1.AssemblyAI({
-    apiKey: process.env.ASSEMBLY_AI,
+import { createServer } from 'http';
+import express from 'express';
+import { WebSocketServer } from 'ws';
+import { AssemblyAI } from 'assemblyai';
+const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+const client = new AssemblyAI({
+    apiKey: process.env.ASSEMBLY_AI || '',
 });
-const transcriber = client.streaming.transcriber({
-    sampleRate: 16000,
-    formatTurns: true,
+app.get('/ping', (req, res) => {
+    // TODO: Count Website Visitors
+    res.send({ text: 'pong' });
 });
-const app = (0, express_1.default)();
-const server = (0, node_http_1.createServer)(app);
-const wss = new ws_1.WebSocketServer({ server });
-wss.on('connection', async function connection(ws) {
-    console.log('client connected');
-    ws.on('error', console.error);
-    ws.on('message', function message(audioBuffer) {
-        transcriber.sendAudio(audioBuffer);
+app.get('/transcript', (req, res) => {
+    wss.on('connection', async function connection(ws) {
+        const transcriber = client.streaming.transcriber({
+            sampleRate: 16000,
+            formatTurns: true,
+        });
+        const MAX_CHUNK = 3200; // In relation to the 16Hz sample rate
+        let MAX_BUFFERED = MAX_CHUNK * 10;
+        let CONNECTED = false;
+        transcriber.on('open', ({ id }) => {
+            console.log(`Session opened with ID:${id}`);
+            CONNECTED = true;
+        });
+        transcriber.on('close', (code, reason) => console.log('Session closed:', code, reason));
+        transcriber.on('turn', (turn) => {
+            console.log('sending output to client');
+            if (!turn.transcript) {
+                return;
+            }
+            ws.send(JSON.stringify({ transcript: turn.transcript }));
+        });
+        transcriber.on('error', (error) => {
+            ws.close();
+            console.error(error);
+        });
+        ws.on('error', console.error);
+        ws.on('message', function message(audioBuffer) {
+            if (CONNECTED && audioBuffer.byteLength <= MAX_BUFFERED) {
+                transcriber.sendAudio(audioBuffer);
+            }
+        });
+        ws.on('close', async () => {
+            console.log('Closing streaming transcript connection');
+            await transcriber.close();
+            CONNECTED = false;
+            console.log('[Assembly AI] stream closed!');
+        });
+        console.log('client connected');
+        // Handling AssemblyAI Transcriber
+        await transcriber.connect();
     });
-    ws.on('close', async () => {
-        console.log('Closing streaming transcript connection');
-        transcriber.close();
-    });
-    transcriber.on('open', ({ id }) => {
-        console.log(`Session opened with ID:${id}`);
-    });
-    transcriber.on('error', (error) => {
-        console.error('Error:', error);
-    });
-    transcriber.on('close', (code, reason) => console.log('Session closed:', code, reason));
-    transcriber.on('turn', (turn) => {
-        if (!turn.transcript) {
-            return;
-        }
-        ws.send(JSON.stringify({ transcript: turn.transcript }));
-    });
-    console.log('Connecting to streaming transcript service');
-    await transcriber.connect();
-    console.log('[AssemblyAI] Connected');
+    res.send({ text: 'Transcription Starting...' });
 });
-server.listen(3000, () => console.log('Started Backend Server'));
-wss.on('listening', () => console.log('[WS] Connection Open'));
+server.listen(3000, () => {
+    console.log('Started Server');
+});
 //# sourceMappingURL=app.js.map
