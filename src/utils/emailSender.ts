7ae609550fd7614781d541fa16c486f224cd7ec0
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-30 11:08:54 +0100

    Updated emailSender.ts

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -4,7 +4,7 @@ import {
 	type EmailTemplateData,
 } from "./emailTemplate.js";
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
 	service: "gmail",
 	auth: {
 		user: process.env.EMAIL_USER,
